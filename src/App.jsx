16dad7b712c04cbe839dc3934c31ec801db9176e
ayush5599 <ayushbharti1999@gmail.com>
2024-07-07 12:44:09 +0530

    Registration Form 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import Form1 from './Form'
+import Form2 from './Form2'
+
+function App() {
+
+  return (
+    <div>
+      <Form2 />
+    </div>
+  )
+}
+
+export default App
